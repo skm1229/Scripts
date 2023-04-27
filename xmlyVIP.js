fe14ac840f4165860198cb8a49f3cea9deb5a77c
404NotFound <104667798+skm1229@users.noreply.github.com>
2023-04-27 21:22:55 +0800

    Update xmlyVIP.js

--- xmlyVIP.js
+++ xmlyVIP.js
@@ -26,7 +26,7 @@ author:   Skm_1229
 hostname = 180.153.*.*,*xima*,*xmcdn*,*.ximalaya.com,*.xmcdn.com,180.153.255.*,180.153.140.*,180.153.250.*,114.80.99.*,114.80.139.237,114.80.161.29,1.62.62.64,51*.com
 
 ************************/
-var List = ["body", "url", "/mobile-playpage/playpage/tabs", "v1/album/track/ts", "business-vip-presale-mobile-web/page/ts", "/product/promotion/v1/album/price/ts", "mobile-user/v2/homePage/", "vip/v1/recommand", "/track/v3/baseInfo", "/product/detail/v1/module/data/dynamic", "/product/detail/v1/basicInfo/dynamic/ts", "indexOf", "\"url\":\"https://t.me/WeiRenQAQ/163", "replace", "\"isAuthorized\":true", "\"text\":\"âï¸èæ¬ç±ä¼äººåè´¹æä¾!è¯·ç¹å»æ­¤å¤è·åææ°JSãå½åæ¯SVIP2çº¿è·¯â¤ï¸", "\"isFree\":true", "\"sampleDuration\":99999", "\"isSample\":true", "\"isPaid\":false", "\"vipPurchaseUrl\":\"https://t.me/WeiRenQAQ", "\"vipPurchaseText\":\"ç¹å»æ·»å ä½èTGé¢é", "\"url\\\":\\\"https://t.me/WeiRenQAQ\\", "\"buttonContent\\\":\\\"â¤ï¸â¤ï¸ç¹å»æ·»å ä½èTGé¢éâ¤ï¸â¤ï¸\\", "userLogoPic\":\"https://static.jietushuiyin.com/uploads/20230204/452be787cb7d63972965498fbf23666d.jpg", "userLevelIcon\":\"http://imagev2.xmcdn.com/group87/M09/0A/4E/wKg5IV8Pwjmw5My3AAASYbQa39Y768.png", "subtitle\":\"2999-09-09åè´¹æä¾ç¦æ­¢è´©å", "vipStatus\":2", "nickName\":\"by~ä¼äºº", "parse", "action", "behaviors", "data", "https://t.me/WeiRenQAQ/163", "vipPurchaseText", "ç¹å»æ·»å ä½èTGé¢é", "labelSubTitle", "æ·»å é¢éè·åææ°èæ¬ç»§ç»­çå¬ï¼ð", "labelText", "è¯¥èæ¬å·²å¤±æ", "stringify", "nickname", "by~ä¼äºº", "isVip", "vipExpireTime", "vipStatus", "vipLevel", "anchorVipInfo", "vipInfo", "level", "mobileLargeLogo", "https://static.jietushuiyin.com/uploads/20230204/452be787cb7d63972965498fbf23666d.jpg", "mobileSmallLogo", "mobileMiddleLogo", "modules", "nickName", "msg", "è¯¥èæ¬ä¸å¤©è¿è¡æ¬¡æ°è¿å¤ãè¯·ä½¿ç¨å«ççº¿è·¯æèéä¸å¤©åæ¥ð"];
+var List = ["body", "url", "/mobile-playpage/playpage/tabs", "v1/album/track/ts", "business-vip-presale-mobile-web/page/ts", "/product/promotion/v1/album/price/ts", "mobile-user/v2/homePage/", "vip/v1/recommand", "/track/v3/baseInfo", "/product/detail/v1/module/data/dynamic", "/product/detail/v1/basicInfo/dynamic/ts", "indexOf", "\"url\":\"https://t.me/+gcEcUFPzQ2g5NTg9", "replace", "\"isAuthorized\":true", "\"text\":\"âï¸èæ¬ç±ä¼äººåè´¹æä¾!è¯·ç¹å»æ­¤å¤è·åææ°JSãå½åæ¯SVIP2çº¿è·¯â¤ï¸", "\"isFree\":true", "\"sampleDuration\":99999", "\"isSample\":true", "\"isPaid\":false", "\"vipPurchaseUrl\":\"https://t.me/WeiRenQAQ", "\"vipPurchaseText\":\"ç¹å»æ·»å ä½èTGé¢é", "\"url\\\":\\\"https://t.me/WeiRenQAQ\\", "\"buttonContent\\\":\\\"â¤ï¸â¤ï¸ç¹å»æ·»å ä½èTGé¢éâ¤ï¸â¤ï¸\\", "userLogoPic\":\"https://static.jietushuiyin.com/uploads/20230204/452be787cb7d63972965498fbf23666d.jpg", "userLevelIcon\":\"http://imagev2.xmcdn.com/group87/M09/0A/4E/wKg5IV8Pwjmw5My3AAASYbQa39Y768.png", "subtitle\":\"2999-09-09åè´¹æä¾ç¦æ­¢è´©å", "vipStatus\":2", "nickName\":\"by~ä¼äºº", "parse", "action", "behaviors", "data", "https://t.me/+gcEcUFPzQ2g5NTg9", "vipPurchaseText", "ç¹å»æ·»å ä½èTGé¢é", "labelSubTitle", "æ·»å é¢éè·åææ°èæ¬ç»§ç»­çå¬ï¼ð", "labelText", "è¯¥èæ¬å·²å¤±æ", "stringify", "nickname", "by~ä¼äºº", "isVip", "vipExpireTime", "vipStatus", "vipLevel", "anchorVipInfo", "vipInfo", "level", "mobileLargeLogo", "https://static.jietushuiyin.com/uploads/20230204/452be787cb7d63972965498fbf23666d.jpg", "mobileSmallLogo", "mobileMiddleLogo", "modules", "nickName", "msg", "è¯¥èæ¬ä¸å¤©è¿è¡æ¬¡æ°è¿å¤ãè¯·ä½¿ç¨å«ççº¿è·¯æèéä¸å¤©åæ¥ð"];
 var Body = $response["body"];
 var Url = $request["url"];
 const URI1 = "/mobile-playpage/playpage/tabs";
@@ -50,33 +50,33 @@ if (Url["indexOf"](URI4) != -1) {
     const _0x4a0323 = "2|4|0|1|3"["split"]("|");
 
     let _0x2d827e = 0;
-    UserBody["data"]["behaviors"][0]["action"]["url"] = "https://t.me/WeiRenQAQ/163";
+    UserBody["data"]["behaviors"][0]["action"]["url"] = "https://t.me/+gcEcUFPzQ2g5NTg9";
     UserBody["data"]["behaviors"][0]["vipPurchaseText"] = "暂无更多信息";
     UserBody["data"]["behaviors"][0]["labelSubTitle"] = "暂无更多信息";
     UserBody["data"]["behaviors"][0]["labelText"] = "暂无更多信息";
-    Body = JSON[List[40]](UserBody);
+    Body = JSON["stringify"](UserBody);
 }
 
 if (Url["indexOf"](URI5) != -1) {
     const _0x4573f8 = "9|10|1|11|2|3|6|7|0|8|4|5"["split"]("|");
 
     let _0x366a73 = 0;
-    UserBody["data"][List[41]] = List[42];
-    UserBody["data"][List[43]] = true;
-    UserBody["data"][List[44]] = 32493834549000;
-    UserBody["data"][List[45]] = 2;
-    UserBody["data"][List[46]] = 5;
-    UserBody["data"][List[47]][List[43]] = true;
-    UserBody["data"][List[48]][List[43]] = true;
-    UserBody["data"][List[48]][List[49]] = 5;
-    UserBody["data"][List[50]] = List[51];
-    UserBody["data"][List[52]] = List[51];
-    UserBody["data"][List[53]] = List[51];
-    Body = JSON[List[40]](UserBody);
+    UserBody["data"]["nickname"] = List[42];
+    UserBody["data"]["isVip"] = true;
+    UserBody["data"]["vipExpireTime"] = 32493834549000;
+    UserBody["data"]["vipStatus"] = 2;
+    UserBody["data"]["vipLevel"] = 5;
+    UserBody["data"]["anchorVipInfo"]["isVip"] = true;
+    UserBody["data"]["vipInfo"]["isVip"] = true;
+    UserBody["data"]["vipInfo"]["level"] = 5;
+    UserBody["data"]["mobileLargeLogo"] = "";
+    UserBody["data"]["mobileSmallLogo"] = "";
+    UserBody["data"]["mobileMiddleLogo"] = "";
+    Body = JSON["stringify"](UserBody);
 }
 
-Url["indexOf"](URI6) != -1 && (delete UserBody["data"][List[54]], UserBody["data"][List[45]] = 2, UserBody["data"][List[55]] = List[42], Body = JSON[List[40]](UserBody));
-Url["indexOf"](URI7) != -1 && (UserBody[List[56]] = List[57], Body = JSON[List[40]](UserBody));
+Url["indexOf"](URI6) != -1 && (delete UserBody["data"][List[54]], UserBody["data"]["vipStatus"] = 2, UserBody["data"][List[55]] = List[42], Body = JSON["stringify"](UserBody));
+Url["indexOf"](URI7) != -1 && (UserBody[List[56]] = List[57], Body = JSON["stringify"](UserBody));
 const _0x50d395 = {
     "body": Body
 };
