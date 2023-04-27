ce4aa5e834925857846b62f243ecd37d874b5130
404NotFound <104667798+skm1229@users.noreply.github.com>
2023-04-27 15:39:01 +0800

    Update wyyVIP.js

--- wyyVIP.js
+++ wyyVIP.js
@@ -8,7 +8,7 @@ hostname = *.music.163.com
 
 [rewrite_local]
 
-^http[s]?:\/\/.+music.+(player\/url|playlist|entrance|\/eapi\/search\/).*$ url script-request-header https://raw.githubusercontent.com/skm1229/Scripts/main/wyyVip.js
+^http[s]?:\/\/.+music.+(player\/url|playlist|entrance|\/eapi\/search\/).*$ url script-request-header https://raw.githubusercontent.com/skm1229/Scripts/main/wyyVIP.js
 
 */
 
