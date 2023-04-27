0dfbe8493fc6fbeda972acc8cfde251f2a82f478
404NotFound <104667798+skm1229@users.noreply.github.com>
2023-04-27 15:45:13 +0800

    Update zhihuToken.js

--- zhihuToken.js
+++ zhihuToken.js
@@ -3,7 +3,7 @@ Headers["Cookie"] = "ff_supports_webp=1;__snaker__id=iCfyKTDTqWYkPa6S;gdxidpyhxd
 Headers["User-Agent"] = "osee2unifiedRelease/13918 osee2unifiedReleaseVersion/9.1.0 Mozilla/5.0 (iPhone; CPU iPhone OS 15_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148";
 Headers["X-MS-ID"] = "D2U0RXatrZWUH1NUweGbqCqO30J6rJqQrQ2zO1NIkbShEX82";
 Headers["Authorization"] = "QzDnhJiEibmqaPaIhL_UeUsL2EJBAZTBLJNVlAgBAAAsgFrx781aBYAAAAAAwQChrM1.2 reraeB"["split"]('')["reverse"]()["join"]('');
-var Json_hearers = {
+var Json_headers = {
     "headers": Headers
 };
-$done(Json_hearers);
+$done(Json_headers);
