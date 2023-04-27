218368e16db939e4c161f20c0eaf5d0e46ef31b4
404NotFound <104667798+skm1229@users.noreply.github.com>
2023-04-27 15:51:31 +0800

    Update xmlyToken.js

--- xmlyToken.js
+++ xmlyToken.js
@@ -1,1 +1,7 @@
-
+var Headers = $request["headers"];
+Headers["Cookie"] = "1&_device=iPhone&216339F3-D51F-42B3-84A3-0C57B7C4047F&9.0.44;1&_token=466770912&D6E99D00340CDF95D69D506EE65F8F07110BF9AB8C264B5CF09115218E74330E902C16CE34F69M8575fac308460C3_;iPhone%2011";
+Headers["User-Agent"] = "ting_v9.1.24_c5(CFNetwork, iOS 15.4, iPhone14,5)";
+var Json_heaers = {
+    "headers": Headers
+};
+$done(Json_headers);
