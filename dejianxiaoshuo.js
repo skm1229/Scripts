14bf0d2e99f395b8702add70173741b6507133b8
404NotFound <104667798+skm1229@users.noreply.github.com>
2023-04-28 20:59:14 +0800

    Update dejianxiaoshuo.js

--- dejianxiaoshuo.js
+++ dejianxiaoshuo.js
@@ -1,1 +1,40 @@
+/***********************************
+function：    得间小说 解锁会员
+author:       Skm_1229
+version:      1.0v
+************************************
+[rewrite_local]
 
+^https:\/\/dj\.palmestore\.com\/zyuc\/api\/user\/accountInfo url script-response-body https://raw.githubusercontent.com/skm1229/Scripts/main/dejianxiaoshuo.js
+
+[mitm]
+
+hostname = dj.palmestore.com
+
+*************************************/
+
+
+var Body = JSON.parse($response.body);
+
+Body.body.userInfo.unlockVipInfo = {
+    "vipType": 1,
+    "vipExpire": 4092599349,
+    "expireDate": "2099-09-09"
+};
+Body.body.userInfo.vipInfo = {
+    "vipType": 1,
+    "vipExpire": 4092599349,
+    "expireDate": "2099-09-09",
+    "halfScreen": "https://dj.palmestore.com/zytc/public/index.php?ca=FreeVip.Index&source=book",
+    "expireDateNew": "2099-09-09",
+    "description": "开通会员",
+    "content": "点击进入购买会员",
+    "fullScreen": "https://dj.palmestore.com/zytc/public/index.php?ca=FreeVip.Index&showContentInStatusBar=1&source=mine"
+
+};
+
+const Json_body = {
+    "body": Body
+};
+
+$done(Json_body);
