abf113a2a326d2c9172faa7b3ca985cd5c1b941b
404NotFound <104667798+skm1229@users.noreply.github.com>
2023-05-04 12:25:12 +0800

    Update wyyVIP.js

--- wyyVIP.js
+++ wyyVIP.js
@@ -11,7 +11,9 @@ hostname = *.music.163.com
 [rewrite_local]
 
 ^http[s]?:\/\/.+music.+(player\/url|playlist|entrance|\/eapi\/search\/).*$ url script-request-header https://raw.githubusercontent.com/skm1229/Scripts/main/wyyToken.js
-^http[s]?:\/\/interface3\.music\.163\.com\/api\/mmusic-vip-membership\/cashier\/info url script-response-body https://raw.githubusercontent.com/skm1229/Scripts/main/wyyVIP.js
+^http[s]?:\/\/(interface3\.music\.163\.com\/api\/mmusic-vip-membership\/cashier\/info) | (music\.163\.com\/weapi\/batch\?csrf_token=ae59ae3f9897880c47cb191c87616064) url script-response-body https://raw.githubusercontent.com/skm1229/Scripts/main/wyyVIP.js
+^http[s]?:\/\/
+
 
 **************************************/
 
@@ -20,6 +22,7 @@ var Url = $request['url'];
 var Skm_1229 = JSON['parse'](Body);
 
 const URI1 = "/api/music-vip-membership/cashier/info";
+const URI2 = "/weapi/batch?csrf_token=ae59ae3f9897880c47cb191c87616064";
 
 if (Url["indexof"](URI1) != -1) {
     //付费音乐包及年费会员
@@ -39,6 +42,18 @@ if (Url["indexof"](URI1) != -1) {
     Body = JSON['stringify'](Skm_1229);
 }
 
+if (Url["indexof"](URI2) != -1) {
+    Skm_1229["/api/music-vip-membership/front/vip/info"]["data"]["associator"]["vipCode"] = 1
+    Skm_1229["/api/music-vip-membership/front/vip/info"]["data"]["associator"]["expireTime"] = 32493834549000;
+    Skm_1229["/api/music-vip-membership/front/vip/info"]["data"]["associator"]["vipLevel"] = "1";
+    Skm_1229["/api/nuser/account/get"]["account"]["vipType"] = 5;
+    Skm_1229["/api/nuser/account/get"]["account"]["paidFee"] = true;
+    Skm_1229["/api/nuser/account/get"]["profile"]["nickname"] = '1111';
+    Skm_1229["/api/nuser/account/get"]["profile"]["vipType"] = 5;
+    Skm_1229["/api/nuser/account/get"]["profile"]["viptypeVersion"] = 1;
+    // Skm_1229["/api/purchased/redvip/vipstatus"]["userVipStatus"] = [4];
+    Body = JSON['stringify'](Skm_1229);
+}
 
 $done({
     "body": Body
