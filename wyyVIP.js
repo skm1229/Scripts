90a836fd7f44bd41401e148f26f242a088f6c613
404NotFound <104667798+skm1229@users.noreply.github.com>
2023-05-04 11:42:21 +0800

    Update wyyVIP.js

--- wyyVIP.js
+++ wyyVIP.js
@@ -11,7 +11,7 @@ hostname = *.music.163.com
 [rewrite_local]
 
 ^http[s]?:\/\/.+music.+(player\/url|playlist|entrance|\/eapi\/search\/).*$ url script-request-header https://raw.githubusercontent.com/skm1229/Scripts/main/wyyToken.js
-^http[s]?:\/\/(interface3\.music\.163\.com)\/api\/music-vip-membership\/cashier\/info.*$ url script-response-body https://raw.githubHeaderscontent.com/skm1229/Scripts/main/wyyVip.js
+^http[s]?:\/\/((interface3\.music\.163\.com)\/api\/music-vip-membership\/cashier\/info).*$ url script-response-body https://raw.githubHeaderscontent.com/skm1229/Scripts/main/wyyVip.js
 
 
 **************************************/
@@ -21,6 +21,7 @@ var Url = $request['url'];
 var Skm_1229 = JSON['parse'](Body);
 
 const URI1 = "/api/music-vip-membership/cashier/info";
+const URI2 = "/weapi/batch?csrf_token=ae59ae3f9897880c47cb191c87616064";
 
 if (Url["indexof"](URI1) != -1) {
     //付费音乐包及年费会员
