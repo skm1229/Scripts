0f2a76c5c81efa4937c7f9fefd1fb3b51d4b0bf2
404NotFound <104667798+skm1229@users.noreply.github.com>
2025-04-22 11:15:09 +0800

    Update WenZhenBaZiPro.js

--- WenZhenBaZiPro.js
+++ WenZhenBaZiPro.js
@@ -6,8 +6,7 @@ author:   skm_1229
 ^https?:\/\/bzpp2\.iwzbz\.com\/api\/.*\/user\/getvipinfo url script-response-body https://raw.githubusercontent.com/skm1229/Scripts/main/WenZhenBaZiPro.js
 
 [mitm] 
-hostname=bzpp2.iwzbz.com
-
+hostname = *.iwzbz.com
 
 
 ***************************************/
@@ -16,11 +15,42 @@ hostname=bzpp2.iwzbz.com
 
 
 
-var objc = JSON.parse($response.body);
-objc.data.adtype = 0;
-objc.data.vipLevel = 3;
-objc.data.vipTipsType = 0;
-objc.data.expires = "2999-11-28 06:06:06";
-$done({
-  body: JSON.stringify(objc)
-});
+// 获取请求地址
+let requestUrl = $request.url;
+let status = isJSON($response.body);
+var obj = status
+  ? JSON.parse(
+      removeExtraSpaces($response.body)
+        .replace(/"isSuccess":\w+/g, '"isSuccess":true')
+        .replace(/"code":-\d+/g, '"code":1')
+    )
+  : $response.body;
+
+function isJSON(str) {
+  try {
+    JSON.parse(str);
+  } catch (e) {
+    return false;
+  }
+  return true;
+}
+
+function removeExtraSpaces(jsonString) {
+  var jsonObj = JSON.parse(jsonString);
+  return JSON.stringify(jsonObj, function (key, value) {
+    if (typeof value === "string") {
+      return value.trim();
+    }
+    return value;
+  });
+}
+
+// 判断是否为匹配项
+if (
+  /^https:\/\/.+\.iwzbz\.com\/api\/v1.1\/user\/getvipinfo?/.test(requestUrl)
+) {
+  obj.data.vipLevel = 3;
+  obj.data.expires = "2222-02-02 22:22:22";
+}
+// 重写数据
+$done({ body: status ? JSON.stringify(obj) : obj });
